f276e80023c011949c89f8f5508e86300caa1dc3
rdor <love-nankai@163.com>
2025-01-12 15:58:56 +0800

    add 24

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module gitee.com/rdor/fairy
 
 go 1.22
 
-require github.com/zrcoder/amisgo v0.0.0-20250108032116-8fa5ab5d3e26
+require github.com/zrcoder/amisgo v0.0.0-20250205095719-ddc054e197bf
--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"gitee.com/rdor/fairy/static"
 
 	"github.com/zrcoder/amisgo"
-	"github.com/zrcoder/amisgo/comp"
 	"github.com/zrcoder/amisgo/conf"
 	"github.com/zrcoder/amisgo/model"
 )
@@ -16,47 +15,50 @@ import (
 const imageCnt = 8
 
 func main() {
-	index := comp.Page().Body(
-		comp.Tabs().TabsMode("sidebar").Swipeable(true).Tabs(
-			comp.Tab().Title("Yaq 1").ID("1").Tab(carousel(1, 8)),
-			comp.Tab().Title("Yaq 2").ID("2").Tab(carousel(2, 13)),
-			comp.Tab().Title("Yaq 3").ID("3").Tab(carousel(3, 16)),
-			comp.Tab().Title("Yaq 4").ID("4").Tab(carousel(4, 17)),
-			comp.Tab().Title("Yaq 5").ID("5").Tab(carousel(5, 30)),
-			comp.Tab().Title("Yaq 6").ID("6").Tab(carousel(6, 7)),
-			comp.Tab().Title("Yaq 7").ID("7").Tab(carousel(7, 19)),
-			comp.Tab().Title("Yaq 8").ID("8").Tab(carousel(8, 11)),
-			comp.Tab().Title("Yaq 9").ID("9").Tab(carousel(9, 7)),
-			comp.Tab().Title("Yaq 10").ID("10").Tab(carousel(10, 6)),
-			comp.Tab().Title("Yaq 11").ID("11").Tab(carousel(11, 9)),
-			comp.Tab().Title("Yaq 12").ID("12").Tab(carousel(12, 12, ".jpeg")),
-			comp.Tab().Title("Yaq 13").ID("13").Tab(carousel(13, 9)),
-			comp.Tab().Title("Yaq 14").ID("14").Tab(carousel(14, 10)),
-			comp.Tab().Title("Yaq 15").ID("15").Tab(carousel(15, 12)),
-			comp.Tab().Title("Yaq 16").ID("16").Tab(carousel(16, 14)),
-			comp.Tab().Title("Yaq 17").ID("17").Tab(carousel(17, 11)),
-			comp.Tab().Title("Yaq 18").ID("18").Tab(carousel(18, 12)),
-			comp.Tab().Title("Yaq 19").ID("19").Tab(carousel(19, 8)),
-			comp.Tab().Title("Yaq 20").ID("20").Tab(carousel(20, 11)),
-			comp.Tab().Title("Yaq 21").ID("21").Tab(carousel(21, 7)),
-			comp.Tab().Title("Yaq 22").ID("22").Tab(carousel(22, 5)),
-			comp.Tab().Title("Yaq 23").ID("23").Tab(carousel(23, 6, ".jpeg")),
-		),
-	)
 	app := amisgo.New(
 		conf.WithTheme(conf.ThemeDark),
 		conf.WithIcon("/static/21/2.webp"),
-	).
-		StaticFS("/static", http.FS(static.FS)).
-		Mount("/", index)
+	)
+	index := app.Page().Body(
+		app.Tabs().TabsMode("sidebar").Swipeable(true).Tabs(
+			app.Tab().Title("Yaq").ID("0").Tab(carousel(app, 0, 1, ".gif")),
+			app.Tab().Title("Yaq 1").ID("1").Tab(carousel(app, 1, 8)),
+			app.Tab().Title("Yaq 2").ID("2").Tab(carousel(app, 2, 13)),
+			app.Tab().Title("Yaq 3").ID("3").Tab(carousel(app, 3, 16)),
+			app.Tab().Title("Yaq 4").ID("4").Tab(carousel(app, 4, 17)),
+			app.Tab().Title("Yaq 5").ID("5").Tab(carousel(app, 5, 30)),
+			app.Tab().Title("Yaq 6").ID("6").Tab(carousel(app, 6, 7)),
+			app.Tab().Title("Yaq 7").ID("7").Tab(carousel(app, 7, 19)),
+			app.Tab().Title("Yaq 8").ID("8").Tab(carousel(app, 8, 11)),
+			app.Tab().Title("Yaq 9").ID("9").Tab(carousel(app, 9, 7)),
+			app.Tab().Title("Yaq 10").ID("10").Tab(carousel(app, 10, 6)),
+			app.Tab().Title("Yaq 11").ID("11").Tab(carousel(app, 11, 9)),
+			app.Tab().Title("Yaq 12").ID("12").Tab(carousel(app, 12, 12, ".jpeg")),
+			app.Tab().Title("Yaq 13").ID("13").Tab(carousel(app, 13, 9)),
+			app.Tab().Title("Yaq 14").ID("14").Tab(carousel(app, 14, 10)),
+			app.Tab().Title("Yaq 15").ID("15").Tab(carousel(app, 15, 12)),
+			app.Tab().Title("Yaq 16").ID("16").Tab(carousel(app, 16, 14)),
+			app.Tab().Title("Yaq 17").ID("17").Tab(carousel(app, 17, 11)),
+			app.Tab().Title("Yaq 18").ID("18").Tab(carousel(app, 18, 12)),
+			app.Tab().Title("Yaq 19").ID("19").Tab(carousel(app, 19, 8)),
+			app.Tab().Title("Yaq 20").ID("20").Tab(carousel(app, 20, 11)),
+			app.Tab().Title("Yaq 21").ID("21").Tab(carousel(app, 21, 7)),
+			app.Tab().Title("Yaq 22").ID("22").Tab(carousel(app, 22, 5)),
+			app.Tab().Title("Yaq 23").ID("23").Tab(carousel(app, 23, 6, ".jpeg")),
+			app.Tab().Title("Yaq 24").ID("24").Tab(carousel(app, 24, 17)),
+		),
+	)
+
+	app.StaticFS("/static", http.FS(static.FS))
+	app.Mount("/", index)
 
 	fmt.Println("Serving on http://localhost:8080")
 	panic(app.Run(":8080"))
 }
 
-func carousel(id, cnt int, f ...string) any {
+func carousel(app *amisgo.App, id, cnt int, f ...string) any {
 	opts := getOptions(id, cnt, f...)
-	return comp.Carousel().
+	return app.Carousel().
 		Height("940").
 		Auto(false).
 		AlwaysShowArrow(true).
